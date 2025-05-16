4a510b6eb7e319f474d099981b21fa3840bde5eb
Christopher-OOC <olojedechristopher24@gmail.com>
2025-05-16 11:26:14 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,74 @@ import "./index.css";
 
 const style = { backgroundColor: "orangered", fontSize: "40px" };
 
+const users = [
+  {
+    id: 1,
+    name: "John Doe",
+    dateOf: "2000-12-3",
+    image: "https://i.pravatar.cc/4566?u=9876",
+    origin: "Nigeria",
+    phoneNumber: "+2349023244556",
+  },
+  {
+    id: 2,
+    name: "Phillip Cool",
+    dateOf: "2005-8-13",
+    image: "https://i.pravatar.cc/4566?u=56799",
+    origin: "England",
+    phoneNumber: "+29023244556",
+  },
+  {
+    id: 3,
+    name: "Ramesh Ravi",
+    dateOf: "2003-3-27",
+    image: "https://i.pravatar.cc/4566?u=45678",
+    origin: "India",
+    phoneNumber: "+803244556",
+  },
+  {
+    id: 4,
+    name: "Martins Orel",
+    dateOf: "2005-8-13",
+    image: "https://i.pravatar.cc/4566?u=97699",
+    origin: "Scotland",
+    phoneNumber: "+290098567856",
+  },
+  {
+    id: 3,
+    name: "Zhou Shi",
+    dateOf: "2010-3-4",
+    image: "https://i.pravatar.cc/4566?u=64678",
+    origin: "China",
+    phoneNumber: "+803244556",
+  },
+];
+
 function App() {
+  const nums = {
+    a: 23,
+    b: 57,
+    c: 98,
+  };
+
+  const { a, b, c } = nums;
+
+  console.log("a b c", a, b, c);
+
+  // const numbers = [1, 2, 3, 4, 5, 6];
+
+  // const numTwice = [];
+
+  // for (let i = 0; i < numbers.length; i++) {
+  //   numTwice[i] = numbers[i] * 2;
+  // }
+
+  // const map = numbers.map((num) => num * 2).filter((num) => num > 8).
+
+  // console.log("numbers", numbers);
+  // console.log("numTwice", numTwice);
+  // console.log("map", map);
+
   return (
     <div>
       <Header />
@@ -42,66 +109,57 @@ function NavLinks() {
   );
 }
 
+const numbers = [1, 2, 3, 4];
+
 function Main() {
   return (
-    <div>
-      Est in exercitation proident laboris elit nulla magna ipsum. Velit
-      deserunt esse esse sit fugiat ex ullamco amet et officia. Esse sint
-      consequat pariatur aliqua non aliquip proident amet. Mollit eu minim eu
-      consectetur aliquip consectetur est consectetur aute commodo minim. Velit
-      voluptate pariatur laboris esse eu fugiat labore ex consequat deserunt
-      deserunt do. Fugiat occaecat tempor eiusmod duis cillum aliqua. Amet do
-      nisi Lorem eiusmod do. Tempor excepteur dolor dolore nisi ut deserunt anim
-      laborum amet. Aliqua non laboris laboris laboris cillum exercitation.
-      Fugiat occaecat sunt dolor minim et duis non qui fugiat tempor irure
-      magna. Nostrud fugiat consequat aliquip consequat et amet magna enim
-      ullamco enim reprehenderit. Irure veniam consequat consectetur adipisicing
-      labore officia commodo. Occaecat cillum labore do amet amet ipsum nostrud.
-      Pariatur nulla in reprehenderit est cillum nulla pariatur do fugiat
-      veniam. Minim anim incididunt ad irure nulla ut et laborum fugiat
-      voluptate tempor aliqua duis. Fugiat aliquip mollit nulla non velit
-      consequat eiusmod consectetur occaecat. Dolore in esse cillum mollit. Esse
-      laborum et aliquip sit sunt ullamco nostrud culpa sit. Voluptate nisi
-      irure eiusmod enim aliqua aute eu. Aliquip exercitation proident officia
-      in exercitation. In qui eiusmod pariatur culpa. Cupidatat aliquip magna
-      nostrud ipsum veniam veniam consequat. Sit sint id exercitation irure
-      eiusmod laborum velit ut voluptate tempor. Elit et aliquip Lorem
-      reprehenderit. Ex deserunt ex aliquip exercitation mollit minim amet duis
-      aliquip ad Lorem. Deserunt veniam enim aliquip nisi in excepteur
-      consectetur laborum culpa nisi occaecat. Sit elit dolor eiusmod dolor
-      velit proident voluptate non fugiat. Laborum aute et commodo ullamco ad
-      veniam non laboris duis dolore nostrud aliquip velit pariatur. Labore
-      dolor ipsum magna aliquip anim amet occaecat consequat nostrud esse.
-      Laborum consequat proident eu adipisicing adipisicing officia aute
-      adipisicing occaecat velit. Anim sint aute aliquip sit qui tempor fugiat
-      dolor commodo magna duis aliqua adipisicing commodo. Commodo mollit minim
-      Lorem sunt labore commodo nostrud adipisicing est nisi. Pariatur occaecat
-      ex consequat fugiat mollit id cillum dolore. Ipsum labore nulla deserunt
-      sit enim eu pariatur occaecat tempor veniam laboris. Exercitation in ut id
-      id minim pariatur pariatur id. Nostrud dolor culpa quis et velit tempor
-      sunt sunt et et ea ad mollit. Do elit nisi magna duis aliquip elit et
-      nostrud proident dolore aliquip quis laborum. Voluptate cillum adipisicing
-      quis exercitation elit dolor nostrud adipisicing excepteur dolore
-      cupidatat proident culpa. Sint elit elit Lorem dolor nisi commodo
-      adipisicing non cillum reprehenderit. Duis id et pariatur aliquip quis
-      laboris ad culpa quis do. Enim irure elit sint tempor amet laborum sunt
-      proident anim laborum sit aliquip ea. Nulla eiusmod aliqua labore fugiat
-      enim ut officia labore minim deserunt anim minim veniam quis. Adipisicing
-      ut proident ad irure ex veniam magna eu nulla consequat aliquip ad.
-      Adipisicing veniam aliquip culpa duis consectetur minim commodo quis.
-      Consectetur laboris adipisicing cupidatat ipsum. Magna ullamco cupidatat
-      consectetur aliqua laboris officia sunt officia. Esse non consequat qui
-      dolor veniam elit commodo. Magna pariatur consequat proident nulla
-      pariatur cupidatat. Deserunt fugiat mollit ut minim ex culpa cillum id
-      aliquip dolor laboris excepteur do exercitation. Esse nulla anim labore
-      nulla sunt aliquip sit veniam. Reprehenderit est do ea Lorem minim nostrud
-      pariatur.
+    <div className="main">
+      <ul className="user__container">
+        {users.map((user) => (
+          <User user={user} school={"school"} />
+        ))}
+      </ul>
     </div>
   );
 }
 
+function User({ user }) {
+  return (
+    <li className="user">
+      <img className="user__image" src={user.image} alt="user-image" />
+      <h4>ID: {user.id}</h4>
+      <p>Name: {user.name}</p>
+      <p>Date of Birth: {user.dateOf}</p>
+      <p>
+        Origin: {user.origin}{" "}
+        {user.origin === "Nigeria" && (
+          <span style={{ backgroundColor: "green" }}>Idan</span>
+        )}
+      </p>
+      <p>Phone Number: {user.phoneNumber}</p>
+    </li>
+  );
+}
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <table className="footer">
+      <thead>
+        <tr>
+          <th>S/N</th>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>John Doe</td>
+          <td>john@com</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 
 // function App() {
